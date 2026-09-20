f97290693757ce935c10207bf1434e1c2fd42f49
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-09-20 17:13:04 +0200

    Refactor swift-contravariant macros around shared type algebra

--- Sources/Contravariant Macro Core/Derivation.swift
+++ Sources/Contravariant Macro Core/Derivation.swift
@@ -3,21 +3,10 @@ public import SwiftSyntax
 import SwiftSyntaxBuilder
 
 public enum Derivation {
-    public static func expansion(of structure: StructDeclSyntax) -> [DeclSyntax] {
+    public static func expansion(of declaration: some DeclGroupSyntax) -> [DeclSyntax] {
         do {
-            let shape = try GenericProduct(structure, arity: 1)
-            let parameters = shape.parameters
-            let fields = shape.properties.fields
-            let forward: [String: String] = [:]
-            let backward: [String: String] = [parameters[0]: "transform"]
-            let arguments = try fields.enumerated().map { index, field in
-                field.name + ": " + (try MappingExpression.apply(shape.fields[index], to: "self.\(field.name)", forward: forward, backward: backward))
-            }.joined(separator: ", ")
-            return [DeclSyntax(stringLiteral: """
-                \(shape.access)func contramap<Mapped>(_ transform: @escaping (Mapped) -> \(parameters[0])) -> \(structure.name.text)<Mapped> {
-                    \(structure.name.text)<Mapped>(\(arguments))
-                }
-                """)]
+            return try Type.Syntax.Mapping.members(of: declaration, method: "contramap",
+                parameters: [.init("Mapped", backward: "transform")])
         } catch { return [DeclSyntax(stringLiteral: "#error(\(String(reflecting: "@Contravariant " + String(describing: error))))")] }
     }
 }
